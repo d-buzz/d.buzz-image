f8b49c88a01ad75e5350c09ad89e0f7f23b850da
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2020-12-07 19:41:47 +0800

    added upload to fleek method

---
+++ src/api/routes/imageRouter.js
@@ -0,0 +1,37 @@
+const { Router } =  require('express')
+const uploadFleek = require('../controllers/upload')
+const multer = require('multer')
+const path = require("path")
+
+
+const storage = multer.diskStorage({
+  destination: './dbuzz',
+  
+  filename: (req, file, cb) => {
+      return cb(null, `file-${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 50 * 1024 * 1024  
+  },
+})
+
+const imageRouter = Router()
+
+imageRouter.post('/upload', upload.single('image'),async (req, res) => {
+  try {
+    const data = req.file
+    const result = await uploadFleek(data)
+
+    res.json(result)
+    
+  } catch (error) {
+    console.log({error})
+  }
+    
+})
+
+module.exports = imageRouter
